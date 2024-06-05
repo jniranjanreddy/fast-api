022bdabff9b75d8c3864a5e363bfef760da97181
Your Name <you@example.com>
2024-06-05 21:58:14 +0530

    GET_POST_DELETE-Working, but PUT Fauling

--- blog/main.py
+++ blog/main.py
@@ -26,16 +26,23 @@ def create(request: schemas.Blog, db: Session = Depends(get_db)):
 
 @app.delete("/blog/{id}", status_code=status.HTTP_204_NO_CONTENT)
 def destroy(id, db: Session = Depends(get_db)):
-    blog = db.query(models.Blog).filter(models.Blog.id == id).delete(synchronize_session=False)
+    blog = db.query(models.Blog).filter(models.Blog.id == id) # .delete(synchronize_session=False)
+    if not blog.first():
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,detail=f"Blog with id {id} not found")
+    blog.delete(synchronize_session=False)
     db.commit()
-    return {"Its destroyed:", id}
+    return {"done"}
     
 
 @app.put("/blog/{id}", status_code=status.HTTP_202_ACCEPTED)
 def update(id, request: schemas.Blog, db: Session = Depends(get_db)):
-    db.query(models.Blog).filter(models.Blog.id == id).update({"title": "updated title"})
-    db.commt()
-    return {"Updated Successfully"}
+    blog = db.query(models.Blog).filter(models.Blog.id == id)
+    # if not blog.first():
+    #     raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,detail=f"Blog with id {id} not found")
+    # blog.update(request)
+    # db.commit()
+    # db.refresh(blog)
+    return {"put request Successfully Completed"}
 
 
 
