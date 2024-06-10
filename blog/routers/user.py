fa73db92b523cbf77a0635c9aa014fcfafb7a934
Your Name <you@example.com>
2024-06-10 14:48:44 +0530

    oauth2-working fine

--- blog/routers/user.py
+++ blog/routers/user.py
@@ -4,6 +4,7 @@ from sqlalchemy.orm import Session
 from fastapi import APIRouter,Depends,status
 from .. hashing import Hash
 from .. repository import user
+from .. import oauth2
 
 get_db = database.get_db
 router = APIRouter(
@@ -12,12 +13,12 @@ router = APIRouter(
 )
 
 @router.post('/', response_model=schemas.ShowUser)
-def create_user(request: schemas.User,db: Session = Depends(get_db)):
+def create_user(request: schemas.User,db: Session = Depends(get_db),currect_user: schemas.User = Depends(oauth2.get_current_user)):
     return user.create(request,db)
     
 
 # GET USERS
 @router.get('/{id}', response_model=schemas.ShowUser)
-def get_user(id: int, db: Session = Depends(get_db)):
+def get_user(id: int, db: Session = Depends(get_db),currect_user: schemas.User = Depends(oauth2.get_current_user)):
     return user.show(id,db)
     
